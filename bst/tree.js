6a49b213910dc5c2a4c865ae517f5189049f34da
Arek <arek.stachyra41@gmail.com>
2022-11-10 19:58:34 +0100

    complete binary search tree

--- bst/tree.js
+++ bst/tree.js
@@ -130,6 +130,46 @@ class Tree {
 		callback(root);
 	}
 
+	height(node) {
+		if (node === null) {
+			return -1;
+		}
+		const leftHeight = this.height(node.left);
+		const rightHeight = this.height(node.right);
+		return Math.max(leftHeight, rightHeight) + 1;
+	}
+
+	// fix this
+	depth(node) {
+		if (node === null) {
+			return 0;
+		}
+		const left = this.depth(node.left);
+		const right = this.depth(node.right);
+		return Math.max(left, right) + 1;
+	}
+
+	isBalanced(root = this.root) {
+		if (root === null) return false;
+
+		const leftHeight = this.height(root.left);
+		const rightHeight = this.height(root.right);
+
+		return Math.abs(leftHeight - rightHeight) < 1;
+	}
+
+	rebalance() {
+		const rebalancedArray = [];
+		this.inorder((item) => {
+			rebalancedArray.push(item.data);
+		});
+		this.root = this.buildTree(
+			rebalancedArray,
+			0,
+			rebalancedArray.length - 1
+		);
+	}
+
 	prettyPrint(node = this.root, prefix = '', isLeft = true) {
 		if (node.right !== null) {
 			this.prettyPrint(
@@ -149,9 +189,55 @@ class Tree {
 	}
 }
 
-const newTree = new Tree([0, 1, 2, 3, 4, 5, 6]);
-newTree.prettyPrint();
-newTree.inorder((item) => {
-	console.log(item.data);
-});
-// console.log(newTree.find(1));
+function logNodeData(node) {
+	console.log(node.data);
+}
+
+function createRandomArray(size, maxNum) {
+	const randomArray = [];
+	for (let i = 0; i < size; i++) {
+		const randomNum = Math.floor(Math.random() * maxNum + 1);
+		randomArray.push(randomNum);
+	}
+	return randomArray;
+}
+
+const randomArray = createRandomArray(10, 100);
+
+const bst = new Tree(randomArray);
+bst.prettyPrint();
+console.log('balanced', bst.isBalanced());
+console.log('level order');
+bst.levelOrder(logNodeData);
+console.log('');
+console.log('preorder');
+bst.preorder(logNodeData);
+console.log('');
+console.log('postorder');
+bst.postorder(logNodeData);
+console.log('');
+console.log('inorder');
+bst.inorder(logNodeData);
+console.log('-------------------------------------------');
+bst.insertNode(120);
+bst.insertNode(420);
+bst.insertNode(2137);
+bst.insertNode(997);
+bst.insertNode(6969);
+bst.prettyPrint();
+console.log('balanced', bst.isBalanced());
+console.log('-------------------------------------------');
+bst.rebalance();
+bst.prettyPrint();
+console.log('balanced', bst.isBalanced());
+console.log('level order');
+bst.levelOrder(logNodeData);
+console.log('');
+console.log('preorder');
+bst.preorder(logNodeData);
+console.log('');
+console.log('postorder');
+bst.postorder(logNodeData);
+console.log('');
+console.log('inorder');
+bst.inorder(logNodeData);
